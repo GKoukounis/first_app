a44956b13db6336b6f1e8ddec3185e75fa6aca9c
George.Koukounis <George.Koukounis@windsorbrokers.com>
2024-03-28 14:10:28 +0200

    Break existing code in a new widget using refactor.

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.red, Colors.blue],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello, World!',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
+        body: NewWidget(),
       ),
     ),
   );
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.red, Colors.blue],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
