0534bdae500cf70932b50992be24ba7c77e6f74c
George.Koukounis <George.Koukounis@windsorbrokers.com>
2024-03-28 16:47:00 +0200

    Different approach of changing the image number

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -11,12 +11,12 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-1.png';
-
+  
+  var currentDiveRoll = 2;
+  
   void rollDice() {
     setState(() {
-      var diceRoll = Random().nextInt(6)+1;
-      activeDiceImage = 'assets/images/dice-$diceRoll.png';  
+      currentDiveRoll = Random().nextInt(6)+1;
     });
   }
   @override
@@ -24,7 +24,7 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Image.asset(activeDiceImage, width: 200, height: 200),
+            Image.asset('assets/images/dice-$currentDiveRoll.png', width: 200, height: 200),
             const SizedBox(height: 20),
             TextButton(
                 onPressed: rollDice,
