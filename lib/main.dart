37399777037227a2156749baba973151f5658165
George.Koukounis <George.Koukounis@windsorbrokers.com>
2024-03-28 12:41:45 +0200

    Configuring widgets and undestading Objects.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // ignore: unnecessary_const
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
+        backgroundColor: Color.fromARGB(255, 42, 0, 114),
+        body: Center(
+          child: Text('Hello, World!'),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
