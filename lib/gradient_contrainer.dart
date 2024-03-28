3cda801c109f0b7aff2488e38e53d1a78299becf
George.Koukounis <George.Koukounis@windsorbrokers.com>
2024-03-28 14:19:13 +0200

    Move widget in a new file and import it in the main.dart

---
+++ lib/gradient_contrainer.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.red, Colors.blue],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:first_app/gradient_contrainer.dart';
 void main() {
   runApp(
     const MaterialApp(
@@ -10,27 +10,3 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.red, Colors.blue],
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
-    );
-  }
-}
